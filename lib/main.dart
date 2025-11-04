23614c0ee999bac2d6ae249d7a723eca1607591b
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-11-04 16:12:12 +0800

    shared pref token check

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sakt/screens/forgetpassword.dart';
 import 'package:sakt/screens/home.dart';
 import 'package:sakt/screens/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,15 +16,57 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  bool isLoggedIn = false;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/logo.png', width: 200),
+                SizedBox(height: 20),
+                CircularProgressIndicator(),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
     return MaterialApp(
-      home: Login(),
+      home: isLoggedIn ? Home() : Login(),
       routes: {
         '/login': (context) => Login(),
         '/forgetpassword': (context) => Forgetpassword(),
         '/home': (context) => Home(),
       },
     );
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('access_token');
+    if (token != null && token.isNotEmpty) {
+      setState(() {
+        isLoggedIn = true;
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 }
