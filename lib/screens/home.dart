f02c6a46a14b5179781df906cb168ff2270672f0
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-11-04 16:00:53 +0800

    logout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,7 +12,21 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await SharedPreferences.getInstance().then((prefs) {
+                prefs.remove('access_token');
+                prefs.remove('name');
+                Navigator.pushReplacementNamed(context, '/login');
+              });
+            },
+          ),
+        ],
+      ),
       body: const Center(child: Text('Welcome to the Home Screen!')),
     );
   }
